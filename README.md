9a20c7af5853bf284ea2febb93c9d4df3a32bb44
skjsjhb <skjsjhb@outlook.com>
2025-07-09 18:54:50 +0800

    feat(build): impl cross-language LTO

--- README.md
+++ README.md
@@ -16,24 +16,23 @@
 
 And by putting them together you can build cooler hybrid apps.
 
-## Important Notes about Compilation
+## Prerequisites
 
 This library pulls saucer (and its dependencies) and compiles them on the fly when building.
-Such workflow makes certain limitations apply:
+Additional tools/libraries are needed for such workflow:
 
-- A nightly Rust compiler is required (refer to [`rust-toolchain.toml`](rust-toolchain.toml) for recommendation).
-- A C++ compiler which supports C++23.
-  Saucer [checks for this](https://github.com/saucer/saucer/blob/v6.0.1/CMakeLists.txt#L37C1-L47C8) when building and
-  an outdated compiler will fail the build.
-- Clang (or `libclang`) [for `bindgen`](https://rust-lang.github.io/rust-bindgen/requirements.html).
+- A nightly Rust compiler (refer to [`rust-toolchain.toml`](rust-toolchain.toml) for recommendation).
+- A C++ compiler which supports C++23. Clang 17+ is recommended as it can also be used to perform cross-language LTO and
+  emit bindings.
+- Clang 9+ or corresponding `libclang` (for `bindgen`).
+- GNU patch (for patching certain source files of saucer).
 - Make sure to have [system dependencies](https://saucer.app/docs/getting-started/dependencies) installed.
-- When building for Windows, MSVC is required.
-- Saucer uses CPM to pull compile-time dependencies. It may take noticeable time to compile this crate during the build
-  process.
+- CMake. For cross-language LTO you also need Ninja.
+- When building for Windows, MSVC is required (MinGW will likely not to work). For cross-language LTO, `clang-cl` and
+  `lld-link` are also required.
 
-This library performs static linking on Windows (with MSVC) and emits a dynamic linked library on all other platforms.
-The emitted library is automatically placed into the target directory can should be shipped with the app using it.
-We're actively working on linking them statically on all platforms.
+Saucer uses CPM to pull compile-time dependencies. It may take noticeable time to compile this crate during the build
+process.
 
 ## Example
 
@@ -116,6 +115,58 @@ fn main() {
 ```
 
 You can use `cargo run --package saucers --bin saucers` to run this example after added it as a dependency.
+
+## Linking
+
+### Static vs. Dynamic
+
+This library can be configured to produce either static or dynamic library.
+Linking to the static library eliminates the need of shipping additional dynamic library files, but will result in
+larger binary unless cross-langauge LTO is enabled (which involves some non-trivial setups).
+
+Enable feature `static-lib` to emit a static library (enabled by default) whenever possible.
+For platforms that's not currently supported, a dynamic library is built as the fallback.
+
+### Cross-Language LTO
+
+One of the many advantages of saucer is its tiny size.
+However, when using static linking, this advantage can be nerved due to the lack of LTO.
+For a simple "Hello World" app, such impact can lead to doubled binary size.
+
+Good news is that you don't always need to switch back to dynamic library for the size!
+By leveraging linker plugins, it's possible to enable LTO even across the language border.
+Follow these steps to begin:
+
+1. Make sure you have Clang and Ninja installed (support of other build systems are in progress).
+   On Windows, `clang-cl` and `lld-link` are also needed in order to link with the MSVC target.
+   Make sure that the Rust compiler has the same LLVM major version as Clang:
+
+   ```shell
+   rustc -vV    # LLVM version: 20.1.5
+   clang -v     # clang version 20.1.7
+   ```
+
+   Add these tools to `PATH` so CMake and Cargo can find it.
+
+2. Enable the `cross-lto` feature in `Cargo.toml` or when compiling.
+   This instructs the build script to toggle certain flags of CMake to enable LTO.
+
+3. Make sure to use LLD as the linker for the Rust part.
+   This can be done by appending the following to `.cargo/config.toml`:
+
+   ```toml
+   [target.x86_64-pc-windows-msvc]
+   rustflags = ["-Clinker-plugin-lto", "-Clinker=lld-link"]
+
+   [target.'cfg(any(target_os = "linux", target_os = "macos"))']
+   rustflags = ["-Clinker-plugin-lto", "-Clinker=clang", "-Clink-arg=-fuse-ld=lld"]
+   ```
+4. Run the build in release mode. You should see the binary size being greatly reduced.
+
+> [!NOTE]
+>
+> On Windows, you might encounter errors when compiling proc-macros.
+> This can be fixed by adding `--target x86_64-pc-windows-msvc` when invoking Cargo.
 
 ## Known Limitations
 
--- build.rs
+++ build.rs
@@ -1,25 +1,45 @@
-use std::path::Path;
 use std::path::PathBuf;
 
 fn main() {
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let profile = std::env::var("PROFILE").unwrap();
+    let is_debug = profile == "debug" || profile == "test";
 
-    let dst = cmake::Config::new("saucer-bindings")
-        .define("SAUCERS_SHARED_LIB", if target_os == "windows" { "OFF" } else { "ON" })
-        .build();
+    let build_static = std::env::var("CARGO_FEATURE_STATIC_LIB").is_ok() && os == "windows";
+    let crs_lto = std::env::var("CARGO_FEATURE_CROSS_LTO").is_ok() && build_static;
+
+    let mut conf = cmake::Config::new("saucer-bindings");
+
+    if build_static {
+        // On Windows, MSVC is required to build static library.
+        if os == "windows" {
+            let target = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
+
+            if target != "msvc" {
+                panic!("MSVC is required to statically link WebView2.");
+            }
+        }
+
+        conf.define("SAUCERS_SHARED_LIB", "OFF");
+    }
+
+    if os == "windows" && crs_lto {
+        conf.generator("Ninja");
+        conf.define("CMAKE_C_COMPILER", "clang-cl");
+        conf.define("CMAKE_CXX_COMPILER", "clang-cl");
+        conf.define("CMAKE_ASM_COMPILER", "clang-cl");
+        conf.define("CMAKE_AR", "llvm-lib");
+
+        if !is_debug {
+            conf.define("CMAKE_INTERPROCEDURAL_OPTIMIZATION", "ON");
+        }
+    }
+
+    let dst = conf.build();
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
-    if target_os == "windows" {
-        let target = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
-
-        if target != "msvc" {
-            panic!("MSVC is required to link to WebView2");
-        }
-
-        let profile = std::env::var("PROFILE").unwrap();
-        let is_debug = profile == "debug" || profile == "test";
-
+    if build_static {
         println!("cargo:rustc-link-lib=static=saucer-bindings");
         println!("cargo:rustc-link-lib=static=saucer");
 
@@ -29,22 +49,23 @@ fn main() {
             println!("cargo:rustc-link-lib=static=fmt");
         }
 
-        println!("cargo:rustc-link-lib=static=WebView2LoaderStatic");
+        if os == "windows" {
+            println!("cargo:rustc-link-lib=static=WebView2LoaderStatic");
 
-        if profile == "debug" || profile == "test" {
-            println!("cargo:rustc-link-lib=dylib=msvcrtd");
+            if is_debug {
+                println!("cargo:rustc-link-lib=dylib=msvcrtd");
+            }
+
+            println!("cargo:rustc-link-lib=dylib=shlwapi");
+            println!("cargo:rustc-link-lib=dylib=gdiplus");
+            println!("cargo:rustc-link-lib=dylib=user32");
+            println!("cargo:rustc-link-lib=dylib=advapi32");
+            println!("cargo:rustc-link-lib=dylib=ole32");
         }
-
-        println!("cargo:rustc-link-lib=dylib=shlwapi");
-        println!("cargo:rustc-link-lib=dylib=gdiplus");
-        println!("cargo:rustc-link-lib=dylib=user32");
-        println!("cargo:rustc-link-lib=dylib=advapi32");
     } else {
         println!("cargo:rustc-link-lib=dylib=saucer-bindings");
-        copy_dylib(&dst);
     }
 
-    println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=saucer-bindings/CMakeLists.txt");
 
     let bindings = bindgen::Builder::default()
@@ -58,39 +79,4 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Failed to emit bindings");
-}
-
-fn copy_dylib(dst: &Path) {
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-
-    let (prefix, suffix) = match target_os.as_str() {
-        "windows" => ("", "dll"),
-        "macos" => ("lib", "dylib"),
-        _ => ("lib", "so")
-    };
-
-    let build_profile = std::env::var("PROFILE").unwrap();
-    let lib_name = format!("{prefix}saucer-bindings.{suffix}");
-
-    let lib_dir = if target_os == "windows" {
-        dst.join("bin")
-    } else {
-        dst.join("lib")
-    };
-
-    let src_lib_path = lib_dir.join(&lib_name);
-
-    let mut dst_paths = Vec::new();
-
-    let exe_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap())
-        .join("target")
-        .join(&build_profile);
-
-    dst_paths.push(exe_dir.join(&lib_name));
-    dst_paths.push(exe_dir.join("deps").join(&lib_name));
-
-    for dst in dst_paths {
-        std::fs::create_dir_all(dst.parent().unwrap()).expect("Failed to create destination directory");
-        std::fs::copy(&src_lib_path, &dst).expect("Failed to copy shared library");
-    }
 }
