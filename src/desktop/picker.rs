d23b62b87981745065ae328ea7f8ce700ef671ba
skjsjhb <skjsjhb@outlook.com>
2025-07-17 15:20:53 +0800

    docs(desktop): update docs of file picker

--- src/desktop/picker.rs
+++ src/desktop/picker.rs
@@ -4,6 +4,7 @@ use std::ptr::NonNull;
 use crate::capi::*;
 use crate::macros::rtoc;
 
+/// Options for picking files or folders.
 pub struct PickerOptions {
     ptr: NonNull<saucer_picker_options>,
     _owns: PhantomData<saucer_picker_options>
@@ -21,6 +22,7 @@ impl Drop for PickerOptions {
 }
 
 impl PickerOptions {
+    /// Creates picker options with default settings.
     pub fn new() -> Self {
         let ptr = unsafe { saucer_picker_options_new() };
         Self {
@@ -29,13 +31,19 @@ impl PickerOptions {
         }
     }
 
+    /// Sets the initial path displayed in the picker.
     pub fn set_initial(&mut self, path: impl AsRef<str>) {
         rtoc!( path => p; saucer_picker_options_set_initial(self.ptr.as_ptr(), p.as_ptr()) );
     }
 
+    /// Adds a filter to the picker.
     pub fn add_filter(&mut self, filter: impl AsRef<str>) {
         rtoc!( filter => f; saucer_picker_options_add_filter(self.ptr.as_ptr(), f.as_ptr()) );
     }
 
     pub(crate) fn as_ptr(&self) -> *mut saucer_picker_options { self.ptr.as_ptr() }
+}
+
+impl Default for PickerOptions {
+    fn default() -> Self { Self::new() }
 }
