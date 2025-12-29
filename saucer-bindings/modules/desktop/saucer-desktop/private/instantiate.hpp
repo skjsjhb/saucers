0c5b045076db4aba6d7ce386ab4deaf4ade27444
skjsjhb <skjsjhb@outlook.com>
2025-12-29 17:11:07 +0800

    chore(*): remove old version

--- saucer-bindings/modules/desktop/saucer-desktop/private/instantiate.hpp
+++
@@ -1,7 +0,0 @@
-#include <saucer/instantiate.hpp>
-
-#define PICKER_INSTANTIATE_IMPL(N, ...)                                                                                     \
-    template picker::result_t<static_cast<picker::type>(N)> desktop::pick<static_cast<picker::type>(N)>(                    \
-        const picker::options &);
-
-#define INSTANTIATE_PICKER() SAUCER_INSTANTIATE(4, PICKER_INSTANTIATE_IMPL, NULL)
--- saucer-bindings/modules/desktop/saucer-desktop/src/desktop.cpp
+++
@@ -1,6 +0,0 @@
-#include "desktop.hpp"
-
-namespace saucer::modules
-{
-    desktop::desktop(saucer::application *parent) : m_parent(parent) {}
-} // namespace saucer::modules
