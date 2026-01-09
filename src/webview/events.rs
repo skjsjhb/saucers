1468d7e6b7e85ea14ab73865ccf14ac63144b37a
skjsjhb <skjsjhb@outlook.com>
2026-01-09 14:43:18 +0800

    chore(*): cleanup listener trait args

--- src/webview/events.rs
+++ src/webview/events.rs
@@ -12,25 +12,27 @@ use crate::status::HandleStatus;
 use crate::url::Url;
 use crate::webview::Webview;
 
+#[allow(unused)] // Template
 pub trait WebviewEventListener {
-    fn on_permission(&self, _webview: Webview, _req: PermissionRequest) -> HandleStatus {
+    fn on_permission(&self, webview: Webview, req: PermissionRequest) -> HandleStatus {
         HandleStatus::Unhandled
     }
-    fn on_fullscreen(&self, _webview: Webview, _is_fullscreen: bool) -> Policy { Policy::Allow }
-    fn on_dom_ready(&self, _webview: Webview) {}
-    fn on_navigated(&self, _webview: Webview, _url: Url) {}
-    fn on_navigate(&self, _webview: Webview, _nav: &Navigation) -> Policy { Policy::Allow }
-    fn on_message(&self, _webview: Webview, _msg: Cow<str>) -> HandleStatus {
+    fn on_fullscreen(&self, webview: Webview, is_fullscreen: bool) -> Policy { Policy::Allow }
+    fn on_dom_ready(&self, webview: Webview) {}
+    fn on_navigated(&self, webview: Webview, url: Url) {}
+    fn on_navigate(&self, webview: Webview, nav: &Navigation) -> Policy { Policy::Allow }
+    fn on_message(&self, webview: Webview, msg: Cow<str>) -> HandleStatus {
         HandleStatus::Unhandled
     }
-    fn on_request(&self, _webview: Webview, _url: Url) {}
-    fn on_favicon(&self, _webview: Webview, _icon: Icon) {}
-    fn on_title(&self, _webview: Webview, _title: String) {}
-    fn on_load(&self, _webview: Webview, _state: LoadState) {}
+    fn on_request(&self, webview: Webview, url: Url) {}
+    fn on_favicon(&self, webview: Webview, icon: Icon) {}
+    fn on_title(&self, webview: Webview, title: String) {}
+    fn on_load(&self, webview: Webview, state: LoadState) {}
 }
 
+#[allow(unused)] // Template
 pub trait WebviewSchemeHandler {
-    fn handle_scheme(&self, _webview: Webview, _req: Request, _exc: Executor) {
-        _exc.reject(SchemeError::NotFound)
+    fn handle_scheme(&self, webview: Webview, req: Request, exc: Executor) {
+        exc.reject(SchemeError::NotFound)
     }
 }
