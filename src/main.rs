4beeab33220b51f6883dd6e0db682cc97dec2ef7
skjsjhb <skjsjhb@outlook.com>
2026-01-09 14:20:06 +0800

    test(*): add tests

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,140 @@
+use std::borrow::Cow;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::sync::Arc;
+
 use saucers::app::App;
+use saucers::app::AppEventListener;
 use saucers::app::AppManager;
 use saucers::app::AppOptions;
-use saucers::app::FinishListener;
+use saucers::policy::Policy;
 use saucers::scheme::register_scheme;
+use saucers::scheme::Executor;
+use saucers::scheme::Request;
+use saucers::scheme::Response;
+use saucers::stash::Stash;
+use saucers::state::LoadState;
+use saucers::status::HandleStatus;
+use saucers::webview::ScriptTime;
 use saucers::webview::Webview;
+use saucers::webview::WebviewEventListener;
 use saucers::webview::WebviewOptions;
+use saucers::webview::WebviewSchemeHandler;
 use saucers::window::Window;
 use saucers::NoOp;
-
-fn start(app: App, fin: &mut FinishListener) {
-    let wnd = Window::new(&app, NoOp).unwrap();
-
-    let opt = WebviewOptions::default();
-
-    let wv = Webview::new(opt, wnd.clone(), NoOp, NoOp, vec!["test".into()]).unwrap();
-
-    wv.set_dev_tools(true);
-    wnd.set_title("Hello");
-    wnd.show();
-
-    fin.set(move |_| {
-        drop(wv);
-        drop(wnd);
-    });
-}
 
 fn main() {
     register_scheme("test");
 
     let app = AppManager::new(AppOptions::new_with_id("hello"));
 
-    app.run(start, NoOp).unwrap();
+    let counter = Arc::new(());
+
+    #[derive(Default)]
+    struct Trace {
+        quit_fired: bool,
+        message_received: bool,
+        load_fired: bool,
+        dom_ready_fired: bool,
+        inject_script_executed: bool,
+    }
+
+    impl Trace {
+        fn verify(&self) {
+            assert!(self.quit_fired, "quit event should be fired");
+            assert!(self.message_received, "message should be received");
+            assert!(self.load_fired, "load event should be fired");
+            assert!(self.dom_ready_fired, "DOM ready event should be fired");
+            assert!(self.inject_script_executed, "inject script should be executed");
+        }
+    }
+
+    #[derive(Clone)]
+    struct SharedTrace {
+        trace: Rc<RefCell<Trace>>,
+    }
+
+    let trace = Rc::new(RefCell::new(Trace::default()));
+
+    impl AppEventListener for SharedTrace {
+        fn on_quit(&self, _app: App) -> Policy {
+            self.trace.borrow_mut().quit_fired = true;
+            Policy::Allow
+        }
+    }
+
+    impl WebviewEventListener for SharedTrace {
+        fn on_dom_ready(&self, webview: Webview) {
+            self.trace.borrow_mut().dom_ready_fired = true;
+            webview.window().close();
+        }
+
+        fn on_message(&self, _webview: Webview, msg: Cow<str>) -> HandleStatus {
+            if msg == "Inject" {
+                self.trace.borrow_mut().inject_script_executed = true;
+            } else if msg == "Hello" {
+                self.trace.borrow_mut().message_received = true;
+            }
+
+            HandleStatus::Handled
+        }
+
+        fn on_load(&self, _webview: Webview, _state: LoadState) {
+            self.trace.borrow_mut().load_fired = true;
+        }
+    }
+
+    let trace_app = SharedTrace { trace: trace.clone() };
+    let trace_webview = trace_app.clone();
+
+    const HTML: &str = r#"
+        <script>
+            window.saucer.internal.message('Hello');
+        </script>
+    "#;
+
+    const URL: &str = "test://some/content";
+
+    struct SchemeHd;
+
+    impl WebviewSchemeHandler for SchemeHd {
+        fn handle_scheme(&self, _webview: Webview, req: Request, exc: Executor) {
+            assert_eq!(req.url().content(), URL, "URL content should be correct");
+            exc.accept(Response::new(Stash::new_view(HTML.as_bytes()), "text/html"));
+        }
+    }
+
+    app.run(
+        {
+            let counter = counter.clone();
+            |app, fin| {
+                let wnd = Window::new(&app, NoOp).unwrap();
+                wnd.show();
+
+                let schemes = vec!["test".into()];
+
+                let wv =
+                    Webview::new(WebviewOptions::default(), wnd, trace_webview, SchemeHd, schemes)
+                        .unwrap();
+
+                wv.set_url_str(URL);
+                wv.inject(
+                    "window.saucer.internal.message('Inject');",
+                    ScriptTime::Ready,
+                    true,
+                    true,
+                );
+
+                fin.set(move |_| {
+                    drop(wv);
+                    drop(counter);
+                });
+            }
+        },
+        trace_app,
+    )
+    .unwrap();
+
+    assert_eq!(Arc::strong_count(&counter), 1, "closures should be dropped");
+    trace.borrow().verify();
 }
